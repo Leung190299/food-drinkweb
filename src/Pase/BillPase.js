0ac1ef64caa59c504e0445764f4beb33f2c4563e
leung190299 <leung01628381807@gmail.com>
2021-03-22 15:34:23 +0700

    đăngky

--- src/Pase/BillPase.js
+++ src/Pase/BillPase.js
@@ -19,17 +19,8 @@ const BillPase=()=> {
         <div className="container-fluid">
        
           <div className="row inner-menu-box">
-            <div className="col-2">
-              <div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-                <label style={{color:'#fff',backgroundColor:'#33ec80ef',textAlign:'center',fontWeight:'bold',fontSize:25}} >Menu</label>
-
-                <Link className="nav-link " exact data-toggle="pill" to="/Bill" >All  </Link>
-                <Link className="nav-link"  data-toggle="pill" to='/Product/edit'>Thêm món </Link>
-                <Link className="nav-link"  data-toggle="pill" to='/Product/Category'>Thể loại </Link>
-                
-              </div>
-            </div>
-            <div className="col-10">
+            
+            <div className="col-12">
                 <Route path='/Bill' exact component={Bills}/>
                 <Route path='/Bill/:id'  component={BillShow}/>
                 
